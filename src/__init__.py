b39f652c86665a595fe6fe501144dfea88f507f6
SDE Intern <intern@hiver.com>
2026-09-13 20:03:07 +0530

    fix: resolve python 3.12 multiprocess resource tracker teardown warning on Windows

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+﻿# Python 3.12 compatibility patch for multiprocess resource tracker on Windows
+try:
+    import multiprocess.resource_tracker as rt
+    def _safe_stop_locked(self, *args, **kwargs):
+        pass
+    rt.ResourceTracker._stop_locked = _safe_stop_locked
+except Exception:
+    pass
